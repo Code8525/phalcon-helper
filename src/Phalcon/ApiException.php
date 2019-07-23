c9cf90fc14a717b16af04a5854e7702e3f584cfe
Code8525 <front-gold@mail.ru>
2019-07-23 09:34:18 +0700

    2

---
+++ src/Phalcon/ApiException.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Phalcon;
+
+/**
+ * Class ApiException
+ * @package Phalcon
+ */
+class ApiException
+{
+
+    public function __construct()
+    {
+
+    }
+
+}
